096b0b58bf8d1de1583b93138b5314d84b23af52
Fitzgerald Kachi <fitzgeraldkachi@gmail.com>
2020-11-29 13:40:21 +0100

    react-redux

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import { Button } from "reactstrap";
+
+function App() {
+  return (
+    <Fragment>
+      <div>
+        <Button color="primary">primary</Button>{" "}
+        <Button color="secondary">secondary</Button>{" "}
+        <Button color="success">success</Button>{" "}
+        <Button color="info">info</Button>{" "}
+        <Button color="warning">warning</Button>{" "}
+        <Button color="danger">danger</Button>{" "}
+        <Button color="link">link</Button>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
